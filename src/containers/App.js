fe1b0c7558a084e9841520eef0810077e4ac0f82
zhi-xuan <rickie120243@gmail.com>
2016-06-27 11:57:53 +0800

    add sample text

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,11 +3,48 @@ import React, {Component} from 'react';
 class App extends Component {
   render() {
     return (
-      <div id="theme" className={this.props.themeStyle}>
-
+      <div id="main">
+        <header id="title">
+          Tibetain-Chinese Sutra Contrast Reading UI on Gocaraparisuddhi
+        </header>
+        <div id="text">
+        <div id="textTop">
+          <div id="textTopTitle" className="textTitle">
+            <span>麗江大藏經</span>
+          </div>
+          <div id="textTopContent" className="textContent">
+            <div id="p">aaaaaa</div>
+            <div id="p">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
+            <div id="p">sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</div>
+            <div id="p">aaaaaa</div>
+            <div id="p">aaaaaa</div>
+          </div>
+        </div>
+        <div id="textMiddle">
+          <div id="textTopTitle" className="textTitle">
+            <span>八十華嚴</span>
+          </div>
+          <div id="textTopContent" className="textContent">
+            aaaaaaaaaaaaaaaa
+            <div id="p">aaaaaa</div>
+            <div id="p">aaaaaa</div>
+            <div id="p">aaaaaa</div>
+          </div>
+        </div>
+        <div id="textBottom">
+          <div id="textTopTitle" className="textTitle">
+            <span>六十華嚴</span>
+          </div>
+          <div id="textTopContent" className="textContent">
+            aaaaaaaaaaaaaaaasssssssssssssss
+            <div id="p">aaaaaa</div>
+            <div id="p">aaaaaa</div>
+            <div id="p">aaaaaa</div>
+          </div>
+        </div>
+        </div>
       </div>
     );
   }
 }
-
 export default App;
