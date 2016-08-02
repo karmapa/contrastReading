b2b32a59589ee1c400d3247e230c51e99ce886e8
zhi-xuan <rickie120243@gmail.com>
2016-08-02 13:59:36 +0800

    data separation

---
+++ assets/data/gocara60.js
@@ -0,0 +1,237 @@
+import React from 'react';
+
+const gocara60 = <span>
+  大方廣佛華嚴經卷第六
+  東晉天竺三藏佛馱跋陀羅譯
+  淨行品第七
+  <tc className="1.1">爾時</tc>，<tc className="1.3">智首</tc><tc className="1.2">菩薩</tc>問<tc className="1.4">文殊師利</tc><tc className="1.5">言</tc>：「<tc className="1.6">佛子</tc>！
+  <tc className="1.8">云何</tc><tc className="1.7">菩薩</tc><tc className="1.9">不染身、口、意業</tc>，<tc className="1.12">不害身、口、意業</tc>，<tc className="1.11">不癡身、
+  口、意業</tc>，<tc className="1.13">不退轉身、口、意業</tc>，<tc className="1.14">不動身、口、意業</tc>，
+  <tc className="1.15">應讚歎身、口、意業</tc>，<tc className="1.16">清淨身、口、意業</tc>，<tc className="1.17">離煩惱身、口、
+  意業</tc>，<tc className="1.18">隨智慧身、口、意業</tc>？云何菩薩<tc className="1.19">生處成就</tc>，
+  <tc className="1.20">姓成就</tc>，<tc className="1.21">家成就</tc>，<tc className="1.22">色</tc><tc className="1.23">相成就</tc>，<tc className="1.24">念成就</tc>，
+  <tc className="1.25">智慧成就</tc>，<tc className="1.26">趣成就</tc>，<tc className="1.27">無畏成就</tc>，
+  <tc className="1.28">覺悟成就</tc>？云何菩薩<tc className="1.30">第一智慧</tc>，<tc className="1.31">最上智慧</tc>，<tc className="1.29">勝智慧</tc>，<tc className="1.32">最勝智慧</tc>，
+  <tc className="1.33">不可量智慧</tc>，<tc className="1.34">不可數智慧</tc>，<tc className="1.35">不可思議智慧</tc>，
+  <tc className="1.37">不可稱智慧</tc>，<tc className="1.38">不可說智慧</tc>？<tc className="1.39">云何菩薩因力具足</tc>，
+  <tc className="1.40">意力具足</tc>，<tc className="1.41">方便力具足</tc>，<tc className="1.42">緣力具足</tc>，<tc className="1.43">境界力具足</tc>，
+  <tc className="1.44">根力具足</tc>，<tc className="1.46">止</tc> <tc className="1.47">觀力具足</tc>，
+  <tc className="1.48">定力具足</tc>？云何菩薩<tc className="1.49">善知陰</tc><tc className="1.50">界</tc><tc className="1.51">入</tc>，<tc className="1.52">善知緣起法</tc>，<tc className="1.53">善知欲</tc>、<tc className="1.54">色</tc>、<tc className="1.55">無色界</tc>，
+  <tc className="1.56">善知過去</tc>、<tc className="1.57">未來</tc>、<tc className="1.58">現在</tc>？云何菩薩修<tc className="1.59">七覺意</tc>，
+  修<tc className="1.66">空</tc>、<tc className="1.67">無相</tc>、<tc className="1.68">無作</tc>？云何菩薩滿足<tc className="1.69">檀波羅蜜</tc>、
+  <tc className="1.70">尸波羅蜜</tc>、<tc className="1.71">羼提波羅蜜</tc>、<tc className="1.72">毘梨耶波羅蜜</tc>、
+  <tc className="1.73">禪波羅蜜</tc>、<tc className="1.74">般若波羅蜜</tc>、<tc className="1.75">慈</tc>、<tc className="1.76">悲</tc>、<tc className="1.77">喜</tc>、
+  <tc className="1.78">捨</tc>？云何菩薩得<tc className="1.79">是處、非處智力</tc>，<tc className="1.80">過去、未來、現在業報智力</tc>，
+  <tc className="1.81">種種諸根智力</tc>，<tc className="1.82">種種性智力</tc>，<tc className="1.83">種種欲智力</tc>，
+  <tc className="1.84">一切至處道智力</tc>，<tc className="1.85">禪定解脫三昧垢淨智力</tc>，
+  <tc className="1.86">宿命無礙智力</tc>，<tc className="1.87">天眼無礙智力</tc>，
+  <tc className="1.88">斷一切煩惱習氣智力</tc>？云何菩薩常為<tc className="1.89">諸天王</tc>守護，恭敬供養；
+  <tc className="1.90">龍王</tc>、<tc className="1.91">鬼神王</tc>、<tc className="1.92">乾闥婆王</tc>、<tc className="1.93">阿脩羅王</tc>、<tc className="1.94">迦樓羅王</tc>、
+  <tc className="1.95">緊那羅王</tc>、<tc className="1.96">摩[目*侯]羅伽王</tc>、<tc className="1.97">人王</tc>、<tc className="1.98">梵天王</tc>等<tc className="1.99">守護</tc>，
+  <tc className="1.100">恭敬供養</tc>？云何菩薩為<tc className="1.113">眾生</tc><tc className="1.101">舍</tc>，<tc className="1.102">為救</tc>，<tc className="1.103">為歸</tc>，
+  <tc className="1.104">為趣</tc>，<tc className="1.105">為炬</tc>，<tc className="1.106">為明</tc>，<tc className="1.107">為燈</tc>，<tc className="1.108">為導</tc>，
+  <tc className="1.111">為</tc><tc className="1.109">無上導</tc>？<tc className="1.124">云何</tc>菩薩於一切眾生<tc className="1.114">為第一</tc>，<tc className="1.115">為大</tc>；<tc className="1.116">為勝</tc>；
+  <tc className="1.120">為上</tc>；<tc className="1.121">為無上</tc>；<tc className="1.122">為無等</tc>；<tc className="1.123">為無等等</tc>？」　爾時，
+  文殊師利答智首菩薩曰：「善哉！善哉！佛子！
+  <tc className="2.1">多所饒益</tc>，<tc className="2.2">多所安隱</tc>，<tc className="2.3">哀愍世間</tc>，<tc className="2.4">惠利一切</tc>，
+  <tc className="2.5">安樂天人</tc>，<tc className="2.6">問如是義</tc>。佛子！菩薩<tc className="2.7">成就身、口、
+  意業</tc>，<tc className="2.8">能得一切勝妙功德</tc>；<tc className="2.9">於佛正法，
+  心無罣礙</tc>；<tc className="2.10">去、來、今佛所轉法輪，能隨順轉</tc>；
+  <tc className="2.11">不捨眾生</tc>，<tc className="2.12">明達實相</tc>；<tc className="2.13">斷一切惡</tc>，<tc className="2.14">具足眾善</tc>；
+  <tc className="2.15">色像第一，悉如普賢大菩薩等</tc>；
+  <tc className="2.16">成就如來一切種智</tc>；<tc className="2.17">於一切法悉得自在</tc>，
+  <tc className="2.18">而為眾生第二尊導</tc>。佛子！何等<tc className="2.19">身、口、意業</tc><tc className="2.20">能得一切勝妙功德</tc>？
+  「<tc className="3.1">菩薩在家</tc>，　　當願眾生，　　<tc className="3.2">捨離家難，
+  入空法中</tc>。　　<tc className="3.3">孝事父母</tc>，　　當願眾生，
+  <tc className="3.4">一切護養，　　永得大安</tc>。　　<tc className="3.5">妻子集會</tc>，
+  當願眾生，　　<tc className="3.6">令出愛獄，　　無戀慕心</tc>。
+  <tc className="3.7">若得五欲</tc>，　　當願眾生，　　<tc className="3.8">捨離貪惑，
+  功德具足</tc>。　　<tc className="3.9">若在妓樂</tc>，　　當願眾生，
+  <tc className="3.10">悉得法樂，　　見法如幻</tc>。　　<tc className="3.11">若在房室</tc>，
+  當願眾生，　　<tc className="3.12">入賢聖地，　　永離欲穢</tc>。
+  <tc className="3.13">著寶瓔珞</tc>，　　當願眾生，　　<tc className="3.14">捨離重擔，
+  度有無岸</tc>。　　<tc className="3.15">若上樓閣</tc>，　　當願眾生，
+  <tc className="3.16">昇佛法堂，　　得微妙法</tc>。　　<tc className="3.17">布施所珍</tc>，
+  當願眾生，　　<tc className="3.18">悉捨一切，　　心無貪著</tc>。
+  <tc className="3.19">若在聚會</tc>，　　當願眾生，　　<tc className="3.20">究竟解脫，
+  到如來處</tc>。　　<tc className="3.21">若在危難</tc>，　　當願眾生，
+  <tc className="3.22">隨意自在，　　無所罣礙</tc>。　　<tc className="3.23">以信捨家</tc>，
+  當願眾生，　　<tc className="3.24">棄捨世業，　　心無所著</tc>。
+  <tc className="3.25">若入僧坊</tc>，　　當願眾生，　　<tc className="3.26">一切和合，
+  心無限礙</tc>。　　<tc className="3.27">詣大小師</tc>，　　當願眾生，
+  <tc className="3.28">開方便門，　　深入法要</tc>。　　<tc className="3.29">求出家法</tc>，
+  當願眾生，　　<tc className="3.30">得不退轉，　　心無障礙</tc>。
+  <tc className="3.31">脫去俗服</tc>，　　當願眾生，　　<tc className="3.32">解道修德，
+  無復懈怠</tc>。　　<tc className="3.33">除剃鬚髮</tc>，　　當願眾生，
+  <tc className="3.34">斷除煩惱，　　究竟寂滅</tc>。　　<tc className="3.35">受著袈裟</tc>，
+  當願眾生，　　<tc className="3.36">捨離三毒，　　心得歡喜</tc>。
+  <tc className="3.37">受出家法</tc>，　　當願眾生，　　<tc className="3.38">如佛出家，
+  開導一切</tc>。　　<tc className="3.39">自歸於佛</tc>，　　當願眾生，
+  <tc className="3.40">體解大道，　　發無上意</tc>。　　<tc className="3.41">自歸於法</tc>，
+  當願眾生，　　<tc className="3.42">深入經藏</tc>，　　智慧如海。
+  <tc className="3.43">自歸於僧</tc>，　　當願眾生，　　<tc className="3.44">統理大眾，
+  一切無礙</tc>。　　<tc className="3.45">受持淨戒</tc>，　　當願眾生，
+  <tc className="3.46">具足修習，　　學一切戒</tc>。　　<tc className="3.47">受行道禁</tc>，
+  當願眾生，　　<tc className="3.48">具足道戒，　　修如實業</tc>。
+  <tc className="3.49">始請和尚</tc>，　　當願眾生，　　<tc className="3.50">得無生智，
+  到於彼岸</tc>。　　<tc className="3.51">受具足戒</tc>，　　當願眾生，
+  <tc className="3.52">得勝妙法，　　成就方便</tc>。　　<tc className="3.53">若入房舍</tc>，
+  當願眾生，　　<tc className="3.54">昇無上堂，　　得不退法</tc>。
+  <tc className="3.55">若敷床座</tc>，　　當願眾生，　　<tc className="3.56">敷善法座，
+  見真實相</tc>。　　<tc className="3.57">正身端坐</tc>，　　當願眾生，
+  <tc className="3.58">坐佛道樹，　　心無所倚</tc>。　　<tc className="3.59">結跏趺坐</tc>，
+  當願眾生，　　<tc className="3.60">善根堅固，　　得不動地</tc>。
+  <tc className="3.61">三昧正受</tc>，　　當願眾生，　　<tc className="3.62">向三昧門，
+  得究竟定</tc>。　　<tc className="3.63">觀察諸法</tc>，　　當願眾生，
+  <tc className="3.64">見法真實，　　無所罣礙</tc>。　　<tc className="3.65">捨跏趺坐</tc>，
+  當願眾生，　　<tc className="3.66">知諸行性，　　悉歸散滅</tc>。
+  <tc className="3.67">下床安足</tc>，　　當願眾生，　　<tc className="3.68">履踐聖跡，
+  不動解脫</tc>。　　<tc className="3.69">始舉足時</tc>，　　當願眾生，
+  <tc className="3.70">越度生死，　　善法滿足</tc>。　　<tc className="3.71">被著衣裳</tc>，
+  當願眾生，　　<tc className="3.72">服諸善根，　　每知慚愧</tc>。
+  <tc className="3.73">整服結帶</tc>，　　當願眾生，　　<tc className="3.74">自檢修道，
+  不壞善法</tc>。　　<tc className="3.75">次著上衣</tc>，　　當願眾生，
+  <tc className="3.76">得上善根，　　究竟勝法</tc>。　　<tc className="3.77">著僧伽梨</tc>，
+  當願眾生，　　<tc className="3.78">大慈覆護，　　得不動法</tc>。
+  <tc className="3.79">手執楊枝</tc>，　　當願眾生，　　<tc className="3.80">心得正法，
+  自然清淨</tc>。　　<tc className="3.81">晨嚼楊枝</tc>，　　當願眾生，
+  <tc className="3.82">得調伏牙，　　噬諸煩惱</tc>。　　<tc className="3.83">左右便利</tc>，
+  當願眾生，　　<tc className="3.84">蠲除污穢，　　無婬怒癡</tc>。
+  <tc className="3.85">已而就水</tc>，　　當願眾生，　　<tc className="3.86">向無上道，
+  得出世法</tc>。　　<tc className="3.87">以水滌穢</tc>，　　當願眾生，
+  <tc className="3.88">具足淨忍，　　畢竟無垢</tc>。　　<tc className="3.89">以水盥掌</tc>，
+  當願眾生，　　<tc className="3.90">得上妙手，　　受持佛法</tc>。
+  <tc className="3.91">澡漱口齒</tc>，　　當願眾生，　　<tc className="3.92">向淨法門，
+  究竟解脫</tc>。　　<tc className="3.93">手執錫杖</tc>，　　當願眾生，
+  <tc className="3.94">設淨施會，　　見道如實</tc>。　　<tc className="3.95">擎持應器</tc>，
+  當願眾生，　　<tc className="3.96">成就法器，　　受天人供</tc>。
+  <tc className="3.97">發趾向道</tc>，　　當願眾生，　　<tc className="3.98">趣佛菩提，
+  究竟解脫</tc>。　　<tc className="3.99">若已在道</tc>，　　當願眾生，
+  <tc className="3.100">成就佛道，　　無餘所行</tc>。　　<tc className="3.101">涉路而行</tc>，
+  當願眾生，　　<tc className="3.102">履淨法界，　　心無障礙</tc>。
+  <tc className="3.103">見趣高路</tc>，　　當願眾生，　　<tc className="3.104">昇無上道，
+  超出三界</tc>。　　<tc className="3.105">見趣下路</tc>，　　當願眾生，
+  <tc className="3.106">謙下柔軟，　　入佛深法</tc>。　　<tc className="3.107">若見險路</tc>，
+  當願眾生，　　<tc className="3.108">棄捐惡道，　　滅除邪見</tc>。
+  <tc className="3.109">若見直路</tc>，　　當願眾生，　　<tc className="3.110">得中正意，
+  身口無曲</tc>。　　<tc className="3.111">見道揚塵</tc>，　　當願眾生，
+  <tc className="3.112">永離塵穢，　　畢竟清淨</tc>。　　<tc className="3.113">見道無塵</tc>，
+  當願眾生，　　<tc className="3.114">大悲所熏，　　心意柔潤</tc>。
+  <tc className="3.115">見深阬澗</tc>，　　當願眾生，　　<tc className="3.116">向正法界，
+  滅除諸難</tc>。　　<tc className="3.117">見聽訟堂</tc>，　　當願眾生，
+  <tc className="3.118">說甚深法，　　一切和合</tc>。　　<tc className="3.119">若見大樹</tc>，
+  當願眾生，　　<tc className="3.120">離我諍心，　　無有忿恨</tc>。
+  <tc className="3.121">若見叢林</tc>，　　當願眾生，　　<tc className="3.122">一切敬禮，
+  天人師仰</tc>。　　<tc className="3.123">若見高山</tc>，　　當願眾生，
+  <tc className="3.124">得無上善，　　莫能見頂</tc>。　　<tc className="3.125">若見刺棘</tc>，
+  當願眾生，　　<tc className="3.126">拔三毒刺</tc>，　　無賊害心。
+  <tc className="3.127">見樹茂葉</tc>，　　當願眾生，　　<tc className="3.128">以道自蔭，
+  入禪三昧</tc>。　　<tc className="3.131">見樹好華</tc>，　　當願眾生，
+  <tc className="3.132">開淨如華，　　相好滿具</tc>。　　<tc className="3.133">見樹豐果</tc>，
+  當願眾生，　　<tc className="3.134">起道樹行，　　成無上果</tc>。
+  <tc className="3.135">見諸流水</tc>，　　當願眾生，　　<tc className="3.136">得正法流，
+  入佛智海</tc>。　　<tc className="3.137">若見陂水</tc>，　　當願眾生，
+  <tc className="3.138">悉得諸佛，　　不壞正法</tc>。　　<tc className="3.139">若見浴池</tc>，
+  當願眾生，　　<tc className="3.140">入佛海智，　　問答無窮</tc>。
+  <tc className="3.141">見人汲井</tc>，　　當願眾生，　　<tc className="3.142">得如來辯，
+  不可窮盡</tc>。　　<tc className="3.143">若見泉水</tc>，　　當願眾生，
+  <tc className="3.144">善根無盡，　　境界無上</tc>。　　<tc className="3.147">見山澗水</tc>，
+  當願眾生，　　<tc className="3.148">洗濯塵垢，　　意解清淨</tc>。
+  <tc className="3.145">若見橋梁</tc>，　　當願眾生，　　<tc className="3.146">興造法橋，
+  度人不休</tc>。　　<tc className="3.149">見修園圃</tc>，　　當願眾生，
+  <tc className="3.150">耘除穢惡，　　不生欲根</tc>。　　<tc className="3.151">見無憂林</tc>，
+  當願眾生，　　<tc className="3.152">心得歡喜，　　永除憂惱</tc>。
+  <tc className="3.153">見好園池</tc>，　　當願眾生，　　<tc className="3.154">勤修眾善，
+  具足菩提</tc>。　　<tc className="3.155">見嚴飾人</tc>，　　當願眾生，
+  <tc className="3.156">三十二相，　　而自莊嚴</tc>。　　<tc className="3.157">見素服人</tc>，
+  當願眾生，　　<tc className="3.158">究竟得到，　　頭陀彼岸</tc>。
+  <tc className="3.159">見志樂人</tc>，　　當願眾生，　　<tc className="3.160">清淨法樂，
+  以道自娛</tc>。　　<tc className="3.161">見愁憂人</tc>，　　當願眾生，
+  <tc className="3.162">於有為法，　　心生厭離</tc>。　　<tc className="3.163">見歡樂人</tc>，
+  當願眾生，　　<tc className="3.164">得無上樂，　　憺怕無患</tc>。
+  <tc className="3.165">見苦惱人</tc>，　　當願眾生，　　<tc className="3.166">滅除眾苦，
+  得佛智慧</tc>。　　<tc className="3.167">見強健人</tc>，　　當願眾生，
+  <tc className="3.168">得金剛身</tc>，　　<tc className="3.169">無有衰耄</tc>。　　<tc className="3.170">見疾病人</tc>，
+  當願眾生，　　<tc className="3.171">知身空寂，　　解脫眾苦</tc>。
+  <tc className="3.172">見端正人</tc>，　　當願眾生，　　<tc className="3.173">歡喜恭敬，
+  諸佛菩薩</tc>。　　<tc className="3.174">見醜陋人</tc>，　　當願眾生，
+  <tc className="3.175">遠離鄙惡，　　以善自嚴</tc>。　　<tc className="3.176">見報恩人</tc>，
+  當願眾生，　　<tc className="3.177">常念諸佛，　　菩薩恩德</tc>。
+  <tc className="3.178">見背恩人</tc>，　　當願眾生，　　<tc className="3.179">常見賢聖，
+  不作眾惡</tc>。　　<tc className="3.180">若見沙門</tc>，　　當願眾生，
+  <tc className="3.181">寂靜調伏，　　究竟無餘</tc>。　　<tc className="3.182">見婆羅門</tc>，
+  當願眾生，　　<tc className="3.183">得真清淨，　　離一切惡</tc>。
+  <tc className="3.184">若見仙人</tc>，　　當願眾生，　　<tc className="3.185">向正真道，
+  究竟解脫</tc>。　　<tc className="3.186">見苦行人</tc>，　　當願眾生，
+  <tc className="3.187">堅固精勤，　　不退佛道</tc>。　　<tc className="3.188">見著甲冑</tc>，
+  當願眾生，　　<tc className="3.189">誓服法鎧，　　得無師法</tc>。
+  <tc className="3.190">見無鎧仗</tc>，　　當願眾生，　　<tc className="3.191">遠離眾惡，
+  親近善法</tc>。　　<tc className="3.192">見論議人</tc>，　　當願眾生，
+  <tc className="3.193">得無上辯，　　摧伏外道</tc>。　　<tc className="3.194">見正命人</tc>，
+  當願眾生，　　<tc className="3.195">得清淨命，　　威儀不異</tc>。
+  <tc className="3.196">若見帝王</tc>，　　當願眾生，　　<tc className="3.197">逮得法王，
+  轉無礙輪</tc>。　　<tc className="3.198">見帝王子</tc>，　　當願眾生，
+  <tc className="3.199">履佛子行，　　化生法中</tc>。　　<tc className="3.200">若見長者</tc>，
+  當願眾生，　　<tc className="3.201">永離愛欲，　　深解佛法</tc>。
+  <tc className="3.202">若見大臣</tc>，　　當願眾生，　　<tc className="3.203">常得正念，
+  修行眾善</tc>。　　<tc className="3.204">若見城郭</tc>，　　當願眾生，
+  <tc className="3.205">得金剛身，　　心不可沮</tc>。　　<tc className="3.206">若見王都</tc>，
+  當願眾生，　　<tc className="3.207">明達遠照，　　功德自在</tc>。
+  <tc className="3.208">若見妙色</tc>，　　當願眾生，　　<tc className="3.209">得上妙色，
+  天人讚歎</tc>。　　<tc className="3.210">入里乞食</tc>，　　當願眾生，
+  <tc className="3.211">入深法界，　　心無障礙</tc>。　　<tc className="3.212">到人門戶</tc>，
+  當願眾生，　　<tc className="3.213">入總持門，　　見諸佛法</tc>。
+  <tc className="3.214">入人堂室</tc>，　　當願眾生，　　<tc className="3.215">入一佛乘，
+  明達三世</tc>。　　<tc className="3.216">遇難持戒</tc>，　　當願眾生，
+  <tc className="3.217">不捨眾善，　　永度彼岸</tc>。　　<tc className="3.218">見捨戒人</tc>，
+  當願眾生，　　<tc className="3.219">超出眾難，　　度三惡道</tc>。
+  <tc className="3.220">若見空缽</tc>，　　當願眾生，　　<tc className="3.221">其心清淨，
+  空無煩惱</tc>。　　<tc className="3.222">若見滿缽</tc>，　　當願眾生，
+  <tc className="3.223">具足成滿，　　一切善法</tc>。　　<tc className="3.224">若得食時</tc>，
+  當願眾生，　　<tc className="3.225">為法供養，　　志在佛道</tc>。
+  <tc className="3.226">若不得食</tc>，　　當願眾生，　　<tc className="3.227">遠離一切，
+  諸不善行</tc>。　　<tc className="3.228">見慚愧人</tc>，　　當願眾生，
+  <tc className="3.229">慚愧正行，　　調伏諸根</tc>。　　<tc className="3.230">見無慚愧</tc>，
+  當願眾生，　　<tc className="3.231">離無慚愧，　　普行大慈</tc>。
+  <tc className="3.232">得香美食</tc>，　　當願眾生，　　<tc className="3.233">知節少欲，
+  情無所著</tc>。　　<tc className="3.234">得不美食</tc>，　　當願眾生，
+  <tc className="3.235">具足成滿，　　無願三昧</tc>。　　<tc className="3.236">得柔軟食</tc>，
+  當願眾生，　　<tc className="3.237">大悲所熏，　　心意柔軟</tc>。
+  <tc className="3.238">得麤澀食</tc>，　　當願眾生，　　<tc className="3.239">永得遠離，
+  世間愛味</tc>。　　<tc className="3.240">若嚥食時</tc>，　　當願眾生，
+  <tc className="3.241">禪悅為食，　　法喜充滿</tc>。　　<tc className="3.242">所食雜味</tc>，
+  當願眾生，　　<tc className="3.243">得佛上味，　　化成甘露</tc>。
+  <tc className="3.244">飯食已訖</tc>，　　當願眾生，　　<tc className="3.245">德行充盈，
+  成十種力</tc>。　　<tc className="3.246">若說法時</tc>，　　當願眾生，
+  <tc className="3.247">得無盡辯，　　深達佛法</tc>。　　<tc className="3.248">退坐出堂</tc>，
+  當願眾生，　　<tc className="3.249">深入佛智，　　永出三界</tc>。
+  <tc className="3.250">若入水時</tc>，　　當願眾生，　　<tc className="3.251">深入佛道，
+  等達三世</tc>。　　<tc className="3.252">澡浴身體</tc>，　　當願眾生，
+  <tc className="3.253">身心無垢，　　光明無量</tc>。　　<tc className="3.254">盛暑炎熾</tc>，
+  當願眾生，　　<tc className="3.255">離煩惱熱，　　得清涼定</tc>。
+  <tc className="3.256">隆寒冰結</tc>，　　當願眾生，　　<tc className="3.257">究竟解脫，
+  無上清涼</tc>。　　<tc className="3.258">諷誦經典</tc>，　　當願眾生，
+  <tc className="3.259">得總持門，　　攝一切法</tc>。　　<tc className="3.260">若見如來</tc>，
+  當願眾生，　　<tc className="3.261">悉得佛眼，　　見諸最勝</tc>。
+  <tc className="3.262">諦觀如來</tc>，　　當願眾生，　　<tc className="3.263">悉睹十方，
+  端正如佛</tc>。　　<tc className="3.264">見佛塔廟</tc>，　　當願眾生，
+  <tc className="3.265">尊重如塔，　　受天人敬</tc>。　　<tc className="3.266">敬心觀塔</tc>，
+  當願眾生，　　<tc className="3.267">尊重如佛，　　天人宗仰</tc>。
+  <tc className="3.268">頂禮佛塔</tc>，　　當願眾生，　　<tc className="3.269">得道如佛，
+  無能見頂</tc>。　　<tc className="3.270">右遶塔廟</tc>，　　當願眾生，
+  <tc className="3.271">履行正路，　　究暢道意</tc>。　　<tc className="3.272">遶塔三匝</tc>，
+  當願眾生，　　<tc className="3.273">得一向意，　　勤求佛道</tc>。
+  <tc className="3.274">讚詠如來</tc>，　　當願眾生，　　<tc className="3.275">度功德岸，
+  歎無窮盡</tc>。　　<tc className="3.276">讚佛相好</tc>，　　當願眾生，
+  <tc className="3.277">光明神德，　　如佛法身</tc>。　　<tc className="3.278">若洗足時</tc>，
+  當願眾生，　　<tc className="3.279">得四神足，　　究竟解脫</tc>。
+  <tc className="3.280">昏夜寢息</tc>，　　當願眾生，　　<tc className="3.281">休息諸行，
+  心淨無穢</tc>。　　<tc className="3.282">晨朝覺悟</tc>，　　當願眾生，
+  <tc className="3.283">一切智覺，　　不捨十方</tc>。
+  「<tc className="3.284">佛子</tc>！是為<tc className="3.285">菩薩</tc><tc className="3.286">身、口、
+  意業</tc><tc className="3.287">能得一切勝妙功德</tc>，<tc className="3.294">諸天</tc>、<tc className="3.289">魔</tc>、<tc className="3.290">梵</tc>、<tc className="3.291">沙門</tc>、<tc className="3.292">婆羅門</tc>、<tc className="3.296">人及</tc><tc className="3.297">非人</tc>、<tc className="3.298">聲聞</tc>、
+  <tc className="3.299">緣覺</tc><tc className="3.300">所不能動</tc>。」
+</span>;
+
+
+export default gocara60;
---
+++ assets/data/gocaraText.js
@@ -0,0 +1,9 @@
+import gocaraLJ from './gocaraLJ.js';
+import gocara80 from './gocara80.js';
+import gocara60 from './gocara60.js';
+
+export {
+  gocaraLJ,
+  gocara80,
+  gocara60
+};
