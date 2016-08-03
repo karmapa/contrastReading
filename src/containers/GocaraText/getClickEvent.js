583bda4489326ce1b075f99116f47fda2012f68b
zhi-xuan <rickie120243@gmail.com>
2016-08-03 13:58:16 +0800

    remove console

--- src/containers/GocaraText/getClickEvent.js
+++ src/containers/GocaraText/getClickEvent.js
@@ -2,7 +2,5 @@ import store from '../../store';
 import {sutraClicked} from '../../reducers/contrast.js';
 
 export function getEvent(e) {
-  console.log(e.target);
   store.dispatch(sutraClicked(e));
-  console.log(store.getState().contrast);
 }
